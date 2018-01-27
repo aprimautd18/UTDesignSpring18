e6816052cf3f66cb1d57dc6c64d335e322098ff0
Jeffrey Neer <ffejhog@users.noreply.github.com>
2018-01-26 19:30:04 -0600

    Inital project setup. May still need tweeking, not 100% sure. Will probably need some sort of glub task to bring the angular client application into the api system so it can be easily deployed onto stuff like azure. Will look into it later

---
+++ ImprovedSchedulingSystemApi/ImprovedSchedulingSystemApi/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace ImprovedSchedulingSystemApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles(); // Allows loading to index.html
+            app.UseStaticFiles(); //Allows the application to use wwwroot for the files
+            app.UseMvc(); //MVC for the api layer
+        }
+    }
+}
