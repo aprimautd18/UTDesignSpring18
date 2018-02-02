8185a2f837f98ff8fb826cc515435d25d12c3c46
Kalee Kuhn <kaleegk26@gmail.com>
2018-02-01 22:31:03 -0600

    Added Customers

--- ImprovedSchedulingSystemApi/ImprovedSchedulingSystemApi/Controllers/CustomerController.cs
+++ ImprovedSchedulingSystemApi/ImprovedSchedulingSystemApi/Controllers/CustomerController.cs
@@ -22,7 +22,7 @@ namespace ImprovedSchedulingSystemApi.Controllers
             testCustomerone.Id = 1;
             testCustomerone.firstName = "Alex";
             testCustomerone.lastName = "Kuhn";
-            testCustomerone.address = "123 street";
+            testCustomerone.address = "123 Street";
             testCustomerone.apptTime = new apptTime();
             testCustomerone.apptTime.startTime = new DateTime(2018,02,01,04,00,00);
             testCustomerone.apptTime.endTime = new DateTime(2018, 02, 01, 05, 00, 00);
@@ -30,12 +30,40 @@ namespace ImprovedSchedulingSystemApi.Controllers
             testCustomertwo.Id = 2;
             testCustomertwo.firstName = "K";
             testCustomertwo.lastName = "Felten";
-            testCustomertwo.address = "123 street";
+            testCustomertwo.address = "123 Street";
             testCustomertwo.apptTime = new apptTime();
             testCustomertwo.apptTime.startTime = new DateTime(2018, 02, 01, 13, 00, 00);
             testCustomertwo.apptTime.endTime = new DateTime(2018, 02, 01, 14, 00, 00);
+            Customer testCustomerthree = new Customer();
+            testCustomerthree.Id = 3;
+            testCustomerthree.firstName = "Mario";
+            testCustomerthree.lastName = "Peach";
+            testCustomerthree.address = "324 Burbon Street";
+            testCustomerthree.apptTime = new apptTime();
+            testCustomerthree.apptTime.startTime = new DateTime(2018, 02, 01, 22, 00, 00);
+            testCustomerthree.apptTime.endTime = new DateTime(2018, 02, 01, 23, 30, 00);
+            Customer testCustomerfour = new Customer();
+            testCustomerfour.Id = 4;
+            testCustomerfour.firstName = "Pit";
+            testCustomerfour.lastName = "Arm";
+            testCustomerfour.address = "4400 Shoulder Alley";
+            testCustomerfour.apptTime = new apptTime();
+            testCustomerfour.apptTime.startTime = new DateTime(2018, 02, 28, 08, 00, 00);
+            testCustomerfour.apptTime.endTime = new DateTime(2018, 02, 28, 09, 00, 00);
+            Customer testCustomerfive = new Customer();
+            testCustomerfive.Id = 5;
+            testCustomerfive.firstName = "Sigh";
+            testCustomerfive.lastName = "Tigress";
+            testCustomerfive.address = "2600 Neer Drive";
+            testCustomerfive.apptTime = new apptTime();
+            testCustomerfive.apptTime.startTime = new DateTime(2018, 03, 26, 14, 00, 00);
+            testCustomerfive.apptTime.endTime = new DateTime(2018, 03, 26, 15, 00, 00);
             testCustomers.Add(testCustomerone);
             testCustomers.Add(testCustomertwo);
+            testCustomers.Add(testCustomerthree);
+            testCustomers.Add(testCustomerfour);
+            testCustomers.Add(testCustomerfive);
+
         }
 
         /// <summary>
