494d1f7af518f0b202e664bdd349f5e138879b0c
Jeffrey Neer <darkshadow10120@gmail.com>
2018-02-16 12:13:00 -0600

    Looking into adding Unit Testing

---
+++ ImprovedSchedulingSystemApi/ImprovedSchedulingSystemApi.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ImprovedSchedulingSystemApi.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImprovedSchedulingSystemApi.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00ad7aba-19be-4ed6-9555-f47c7adeb137")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
