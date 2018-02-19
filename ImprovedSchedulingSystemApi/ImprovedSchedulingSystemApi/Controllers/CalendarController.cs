7e0cff95904212be964520a1bfac42e9dfab3a00
Jeffrey Neer <darkshadow10120@gmail.com>
2018-02-18 18:56:10 -0600

    Added new weekLookup endpoint for K.
    "Can y'all set up an endpoint that will accept a date/time object, and return an array of 7 dates from Sunday - Saturday, where the date we give you is contained within the Sunday- Saturday. We may not give you the correct first day."

--- ImprovedSchedulingSystemApi/ImprovedSchedulingSystemApi/Controllers/CalendarController.cs
+++ ImprovedSchedulingSystemApi/ImprovedSchedulingSystemApi/Controllers/CalendarController.cs
@@ -19,7 +19,13 @@ namespace ImprovedSchedulingSystemApi.Controllers
 
         CalendarAccessor db = new CalendarAccessor();
 
-    
+        /// <summary>
+        /// Returns a list of calendar objects based on the date given to lookup
+        /// </summary>
+        /// <param name="calName">The name of the calendar to retreive the dates from</param>
+        /// <param name="startTime">The Date to do the lookup with</param>
+        /// <param name="range">The number of days to return(Defaults to 1)</param>
+        /// <returns></returns>
         [Produces("application/json")]
         [HttpGet("dateLookup")]
         public IActionResult dateLookup( [FromQuery] string calName, [FromQuery] string startTime, [FromQuery] int range)
@@ -57,6 +63,71 @@ namespace ImprovedSchedulingSystemApi.Controllers
                 }
             
                
+            }
+
+            return Ok(data);
+        }
+
+        /// <summary>
+        /// Retuns a list of Calendar objects from Sunday-Saturday. The given date will be one of the days within the week of dates to return
+        /// </summary>
+        /// <param name="calName">The name of the calendar to retreive the dates from</param>
+        /// <param name="startTime">The Date to do the lookup with</param>
+        /// <returns>A list of 7 Calendar objects for the week Sunday-Saturday</returns>
+        [Produces("application/json")]
+        [HttpGet("weekLookup")]
+        public IActionResult dateLookupByContainedDay([FromQuery] string calName, [FromQuery] string startTime)
+        {
+            DateTime starttimeDateTime;
+
+
+            DateTime.TryParse(startTime, CultureInfo.InvariantCulture,
+                DateTimeStyles.None,
+                out starttimeDateTime);
+
+
+            if (calName == null || starttimeDateTime == DateTime.MinValue)
+            {
+                return BadRequest();
+            }
+
+            //list to store the returned values
+            List<dateLookup_CalendarViewModel> data = new List<dateLookup_CalendarViewModel>();
+
+            switch (starttimeDateTime.DayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    break;
+                case DayOfWeek.Monday:
+                    starttimeDateTime = starttimeDateTime.AddDays(-1);
+                    break;
+                case DayOfWeek.Tuesday:
+                    starttimeDateTime = starttimeDateTime.AddDays(-2);
+                    break;
+                case DayOfWeek.Wednesday:
+                    starttimeDateTime = starttimeDateTime.AddDays(-3);
+                    break;
+                case DayOfWeek.Thursday:
+                    starttimeDateTime = starttimeDateTime.AddDays(-4);
+                    break;
+                case DayOfWeek.Friday:
+                    starttimeDateTime = starttimeDateTime.AddDays(-5);
+                    break;
+                case DayOfWeek.Saturday:
+                    starttimeDateTime = starttimeDateTime.AddDays(-6);
+                    break;
+                default:
+                    return BadRequest();
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                CalendarModel test = db.dateLookup(calName, starttimeDateTime.AddDays(i));
+                if (test != null) // Check if the date exists in the calender
+                {
+                    data.Add(new dateLookup_CalendarViewModel(test));
+                }
+
             }
 
             return Ok(data);
