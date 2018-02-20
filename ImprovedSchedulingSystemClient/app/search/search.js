7151351906680f6fb3e2be8115c295182c8defcf
Alex Felten <afeltenxx@gmail.com>
2018-02-20 13:31:29 -0600

    Search by calendar and date works

--- ImprovedSchedulingSystemClient/app/search/search.js
+++ ImprovedSchedulingSystemClient/app/search/search.js
@@ -16,22 +16,24 @@ angular.module('myApp.search', ['ngRoute'])
 var app = angular.module('search', []);
 app.controller('searchCtrl', function($scope,$http) {
     $scope.input = "searchHere";
-    $scope.searchText = "Literally Everything";
-    $scope.output = [];
+    $scope.searchCalendar = "Kelly 441";
+    $scope.searchDate = new Date("2024-12-09");
+    $scope.searchResults = [];
+    $scope.calendarNames = ["Kelly 441", "Ablasaeu 376"];
 
     $scope.searchEngine = function () {
         console.log("in here");
-        $http.get("https://seniordesign2018dev.azurewebsites.net/api/Calendar/dateLookup?calName=Ablaseau%20376&startTime=2023-10-09%2000%3A00%3A00&range=1")
+        //$scope.searchDate = $scope.searchDate.toLocaleDateString();
+        var dummyDate = $scope.searchDate.toISOString();
+        console.log(dummyDate);
+        $http.get("https://seniordesign2018dev.azurewebsites.net/api/Calendar/dateLookup?calName=" + $scope.searchCalendar + "&startTime=" + dummyDate + "&range=1")
             .then(function (response) {
-                console.log("im here bi");
-                //console.log(response.data);
+                console.log("im here biytg");
                 var appointments = (response.data);
                 console.log("Appointments");
                 console.log(appointments);
                 $scope.searchResults = appointments[0].appointments;
-
             });
-
     }
 });
 
