4591ac4fe9f03803ecca06aa51c5eedb8bf4eef9
Alex Felten <afeltenxx@gmail.com>
2018-03-08 15:24:29 -0600

    Very basic customer add functionality implemented

--- ImprovedSchedulingSystemClient/app/search/search.js
+++ ImprovedSchedulingSystemClient/app/search/search.js
@@ -43,6 +43,7 @@ app.controller('searchCtrl', function($scope,$http) {
             });
     }
 
+
     $scope.customerSearchEngine = function () {
         console.log("searching customers");
         $scope.apptSearch = false;
@@ -64,3 +65,47 @@ app.controller('searchCtrl', function($scope,$http) {
     }
 });
 
+app.controller('dialogService',function($scope, $http) {
+    // Get the modal
+    var modal = document.getElementById('addModal');
+
+// Get the button that opens the modal
+    var btn = document.getElementById("addButton");
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+    $scope.buttonPressed = function() {
+        modal.style.display = "block";
+    }
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    $scope.newPatientFirstName = "";
+    $scope.newPatientLastName = "";
+    $scope.newPatientPhoneNumber = "";
+
+    $scope.addNewPatient = function () {
+        console.log("Adding the patient now");
+        var newPatient = {
+            "firstName" : $scope.newPatientFirstName,
+            "lastName" : $scope.newPatientLastName,
+            "phoneNumber" : $scope.newPatientPhoneNumber
+        };
+        $http.post("https://seniordesign2018dev.azurewebsites.net/api/Customer/addCustomer", newPatient)
+            .then(function (response) {
+                console.log(response);
+            })
+    };
+
+});
