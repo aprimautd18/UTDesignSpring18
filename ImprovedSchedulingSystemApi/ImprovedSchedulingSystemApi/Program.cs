4fd1ab807c883bf6bc07d31e94f1a58a994240c0
Jeffrey Neer <darkshadow10120@gmail.com>
2018-03-08 17:08:16 -0600

    Revert

--- ImprovedSchedulingSystemApi/ImprovedSchedulingSystemApi/Program.cs
+++ ImprovedSchedulingSystemApi/ImprovedSchedulingSystemApi/Program.cs
@@ -19,7 +19,6 @@ namespace ImprovedSchedulingSystemApi
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
