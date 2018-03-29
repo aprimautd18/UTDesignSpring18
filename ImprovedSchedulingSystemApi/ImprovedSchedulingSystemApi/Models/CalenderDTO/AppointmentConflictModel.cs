e4a770095d57b5735f9a4fe5e7f19251238106a3
Jeffrey Neer <darkshadow10120@gmail.com>
2018-03-29 14:51:19 -0500

    Appointment conflict model created

---
+++ ImprovedSchedulingSystemApi/ImprovedSchedulingSystemApi/Models/CalenderDTO/AppointmentConflictModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImprovedSchedulingSystemApi.Models.CalenderDTO
+{
+    public class AppointmentConflictModel
+    {
+        public AppointmentModel firstAppointment { get; set; }
+        public AppointmentModel secondAppointment { get; set; }
+    }
+}
