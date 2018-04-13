ca66538d16916b4ed39eab16abbe06c1980d93f3
Jeffrey Neer <darkshadow10120@gmail.com>
2018-04-12 21:39:19 -0500

    Hid view1. Its still there, its just hidden

--- ImprovedSchedulingSystemClient/app/app.js
+++ ImprovedSchedulingSystemClient/app/app.js
@@ -12,5 +12,5 @@ angular.module('myApp', [
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/view2'});
 }]);
